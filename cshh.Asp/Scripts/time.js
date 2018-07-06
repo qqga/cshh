4b1ce857fe66ee237862f5ca74254695672efab8
qqga <qqga@mail.ru>
2018-07-06 23:04:41 +0500

    Animated timepiece on index page.

---
+++ cshh.Asp/Scripts/time.js
@@ -0,0 +1,14 @@
+﻿(function clock(timeId, dateId, interval) {
+
+    var timeEl = document.getElementById(timeId);
+    var dateEl = document.getElementById(dateId);
+    var timer = setInterval(timerElapsed, interval);
+
+    function timerElapsed() {
+        var dt = new Date();
+        if (timeEl)
+            timeEl.innerHTML = dt.toLocaleTimeString();
+        if (dateEl)
+            dateEl.innerHTML = dt.toLocaleDateString();
+    }
+})("timeDisplay", "dateDisplay", 1000);
